02fc7227d4526e5a4072f94b5d29e8ae305fc261
Fernando Galvão <fernando.galvao77@outlook.com>
2021-03-24 12:49:35 -0300

    realizando testes com Map e seus recursos

---
+++ src/main/kotlin/me/fgalvao77/TesteMap.kt
@@ -0,0 +1,20 @@
+package me.fgalvao77
+
+fun main() {
+    //criando a função "Pair" - chave e valor
+    val pair: Pair<String, Double> = Pair("Fernando", 1580.0)
+    val map1 = mapOf(pair)
+
+    //imprimindo o "map1"
+    map1.forEach { (k, v) -> println("Chave: $k | Valor: $v") }
+
+    println("--------------------------------------")
+    //utilizando o recurso "infix"
+    val map2 = mapOf(
+        "Kátia" to 2385.0,
+        "Sara" to 3157.0
+    )
+
+    //imprimindo o "map2"
+    map2.forEach { (k, v) -> println("Chave: $k \t| \tValor: $v")}
+}
