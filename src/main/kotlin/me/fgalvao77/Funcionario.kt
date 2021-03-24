33598c273aecfa6057f9d9d691e07f4e60cbf82a
Fernando Galvão <fernando.galvao77@outlook.com>
2021-03-24 11:35:33 -0300

    separando o "data class" em um arquivo separado "Funcionario", para facilitar nossas manipulações

---
+++ src/main/kotlin/me/fgalvao77/Funcionario.kt
@@ -0,0 +1,19 @@
+package me.fgalvao77
+
+//criando um "data class" para "Funcionario"
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val dataNasc: String,
+    val tipoContratacao: String
+) {
+
+    override fun toString(): String = //realizando interpolação
+        """
+            ---------------------
+            Nome   : $nome
+            Salario: $salario
+            Data de nascimento: $dataNasc
+            ---------------------
+        """.trimIndent()
+}
--- src/main/kotlin/me/fgalvao77/TesteList.kt
+++ src/main/kotlin/me/fgalvao77/TesteList.kt
@@ -29,20 +29,21 @@ fun main() {
         .forEach { println(it) }
 }
 
-//criando um "data class" para "Funcionario"
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val dataNasc: String,
-    val tipoContratacao: String
-    ) {
-
-    override fun toString(): String = //realizando interpolação
-        """
-            ---------------------
-            Nome   : $nome
-            Salario: $salario
-            Data de nascimento: $dataNasc
-            ---------------------
-        """.trimIndent()
-}
+//extraído para um arquivo separado
+////criando um "data class" para "Funcionario"
+//data class Funcionario(
+//    val nome: String,
+//    val salario: Double,
+//    val dataNasc: String,
+//    val tipoContratacao: String
+//    ) {
+//
+//    override fun toString(): String = //realizando interpolação
+//        """
+//            ---------------------
+//            Nome   : $nome
+//            Salario: $salario
+//            Data de nascimento: $dataNasc
+//            ---------------------
+//        """.trimIndent()
+//}
